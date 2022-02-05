ba8f8280bc84feb4151a9b01adcd6814e5e240a8
keyzen <alonso.anavalon@gmail.com>
2022-02-05 02:20:23 -0300

    book

--- public/sw.js
+++ public/sw.js
@@ -22,7 +22,21 @@ this.addEventListener("install", evt => {
                 '/images/man.png',
                 'https://fonts.googleapis.com/css2?family=Kanit:wght@800;900&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap',
                 '/instruments/tejasLee/Tejas Lee - Y.jpg',
-                '/instruments/tejasLee/Tejas Lee.wav'
+                '/instruments/tejasLee/Tejas Lee.wav',
+                '/instruments/tejasLee/book1.jpg',
+                '/instruments/tejasLee/book2.jpg',
+                '/instruments/tejasLee/book3.jpg',
+                '/instruments/tejasLee/book4.jpg',
+                '/instruments/tejasLee/book5.jpg',
+                '/instruments/tejasLee/book6.jpg',
+                '/instruments/tejasLee/book7.jpg',
+                '/instruments/tejasLee/book8.jpg',
+                '/instruments/tejasLee/book9.jpg',
+                '/instruments/tejasLee/book10.jpg',
+                '/instruments/tejasLee/book11.jpg',
+                '/instruments/tejasLee/book12.jpg',
+                '/instruments/tejasLee/book13.jpg',
+                '/instruments/tejasLee/book14.jpg'
 
             ])
         })
