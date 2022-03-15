f97f4f90aae9ffe6f113648c0fba832400e6b9e8
k3yzen <alonso.anavalon@gmail.com>
2022-03-14 22:03:45 -0300

    offlinesdq

--- public/sw.js
+++ public/sw.js
@@ -20,6 +20,7 @@ this.addEventListener("install", evt => {
                 '/tejaslee',
                 '/calculo',
                 '/excel',
+                '/sdq',
                 'https://cdnjs.cloudflare.com/ajax/libs/hamburgers/1.1.3/hamburgers.min.css',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
                 '/images/man.png',
