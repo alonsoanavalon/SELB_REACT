9bf21ce61c611cb517c28e6cd225cdcd0057ebec
k3yzen <alonso.anavalon@gmail.com>
2022-04-22 09:04:51 -0400

    cache version 6

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,11 @@
-let cacheData = "app-v5";
+let cacheData = "app-v6";
 this.addEventListener("install", evt => {
     console.log("installing")
     caches.delete('app-v1')
     caches.delete('app-v2')
     caches.delete("app-v3")
     caches.delete("app-v4")
+    caches.delete("app-v5")
     evt.waitUntil(
         caches.open(cacheData).then((cache) => {
             cache.addAll([
