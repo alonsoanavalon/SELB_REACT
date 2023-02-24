04710e776b857d4203129c81f24ee80ece6d268d
k3yzen <alonso.anavalon@gmail.com>
2023-02-24 18:29:26 -0300

    ocultando desarrollo

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -65,7 +65,7 @@ export default function Aside() {
 
                 }
 
-{
+{/* {
                     userData.role === "Admin" &&
                     <NavLink className={({isActive}) => isActive ? 'active' : ''}to="/desarrollo">
                             <svg id="development-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -75,7 +75,7 @@ export default function Aside() {
                         Desarrollo
                     </NavLink>
 
-                }
+                } */}
 
                 
 
