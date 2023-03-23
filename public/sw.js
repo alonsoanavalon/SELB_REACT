2b8fb689a68ce22db2590b8443398f6d87709aa0
k3yzen <alonso.anavalon@gmail.com>
2023-03-23 17:07:46 -0300

    primer sonido fono guardado local prueba offline parte 2 ep 8

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-let cacheData = "app-v8";
+let cacheData = "app-v9";
 this.addEventListener("install", evt => {
     console.log("installing")
 
@@ -9,8 +9,8 @@ this.addEventListener("install", evt => {
     caches.delete("app-v5")
     caches.delete("app-v6")
     caches.delete("app-v7")
-    // caches.delete("app-v8")
-    caches.delete("app-v9")
+    caches.delete("app-v8")
+    // caches.delete("app-v9")
     caches.delete("app-v10")
     caches.delete("app-v11")
 
