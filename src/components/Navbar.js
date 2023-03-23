ffd1d82d804cf1f36fe5e8907021b8822e156369
k3yzen <alonso.anavalon@gmail.com>
2023-03-23 17:26:57 -0300

    mostrando flor offline 3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -73,7 +73,7 @@ export default function NavBar() {
         const textInfo = `
         Version: 1.2 ||
         Ultima Actualización: 23/03/2023 ||
-        Cache: v9
+        Cache: v11
         `
         alert.show(textInfo, {
             type:'success'
