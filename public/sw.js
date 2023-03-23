6678a6fdca39212ae48c78e70490e263dd76afdd
k3yzen <alonso.anavalon@gmail.com>
2023-03-23 15:58:17 -0300

    primer sonido fono guardado local prueba offline parte 2 ep 1

--- public/sw.js
+++ public/sw.js
@@ -62,6 +62,9 @@ this.addEventListener("install", evt => {
                 '/images/play-button.png',
                 '/images/corazon-2.png',
                 './sounds/go.mp3',
+                // algo de estas mierdas me esta cagando el offline.
+                // './sounds/item0.mp3',
+                // '/sounds/item0.mp3',
                 'https://fonts.googleapis.com/css2?family=Kanit:wght@800;900&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap',
                 "https://res.cloudinary.com/keyzen/image/upload/v1645727162/selb/tejaslee/book/01_pzeieo.jpg",
                 "https://res.cloudinary.com/keyzen/image/upload/v1645727162/selb/tejaslee/book/02_kphnm6.jpg",
