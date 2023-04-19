ce3fe4b0cfcd74a782be394c1a1e469203ab26a6
k3yzen <alonso.anavalon@gmail.com>
2023-04-19 16:22:32 -0400

    test DataTable OK

--- src/components/Desarrollo.js
+++ src/components/Desarrollo.js
@@ -1,23 +1,100 @@
-import { useState, Fragment } from 'react'
+import "bootstrap/dist/css/bootstrap.css";
+import { Button, Col, Row, Table } from "react-bootstrap";
+import {
+  DatatableWrapper,
+  Filter,
+  Pagination,
+  PaginationOptions,
+  TableBody,
+  TableHeader
+} from "react-bs-datatable";
 
-export default function Desarrollo(){
-    return(
-        <Fragment>
-            <h3 style={{ margin: "2rem"}}>Test en etapa de prueba</h3>
-            <ul>
-                <li>
-                    <a className="btn btn-success" href="/corsi">Corsi</a>
-                </li>
-                <li>
-                    <a className="btn btn-success" href="/hnf">HNF</a>
-                </li>
-                <li>
-                    <a className="btn btn-success" href="/fonologico">Fonologico</a>
-                </li>
-                
-                
-            </ul>
-        </Fragment>
-    );
+import TABLE_BODY from "./data.json";
 
+
+// Create table headers consisting of 4 columns.
+const STORY_HEADERS = [
+  {
+    prop: "name",
+    title: "Name",
+    isFilterable: true
+  },
+  {
+    prop: "username",
+    title: "Username"
+  },
+  {
+    prop: "location",
+    title: "Location"
+  },
+  {
+    prop: "date",
+    title: "Last Update"
+  },
+  {
+    prop: "score",
+    title: "Score",
+    isSortable: true
+  },
+  {
+    prop: "button",
+    cell: (row) => (
+      <Button
+        variant="outline-primary"
+        size="sm"
+        onClick={() => {
+          alert(`${row.username}'s score is ${row.score}`);
+        }}
+      >
+        Click me
+      </Button>
+    )
+  }
+];
+const STORY_BODY = TABLE_BODY;
+
+// Then, use it in a component.
+export default function Desarrollo() {
+  return (
+    <DatatableWrapper
+      body={STORY_BODY}
+      headers={STORY_HEADERS}
+      paginationOptionsProps={{
+        initialState: {
+          rowsPerPage: 10,
+          options: [5, 10, 15, 20]
+        }
+      }}
+    >
+      <Row className="mb-4 p-2">
+        <Col
+          xs={12}
+          lg={4}
+          className="d-flex flex-col justify-content-end align-items-end"
+        >
+          <Filter />
+        </Col>
+        <Col
+          xs={12}
+          sm={6}
+          lg={4}
+          className="d-flex flex-col justify-content-lg-center align-items-center justify-content-sm-start mb-2 mb-sm-0"
+        >
+          <PaginationOptions />
+        </Col>
+        <Col
+          xs={12}
+          sm={6}
+          lg={4}
+          className="d-flex flex-col justify-content-end align-items-end"
+        >
+          <Pagination />
+        </Col>
+      </Row>
+      <Table>
+        <TableHeader />
+        <TableBody />
+      </Table>
+    </DatatableWrapper>
+  );
 }
