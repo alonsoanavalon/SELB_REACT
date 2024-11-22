4b65edb8d7ada43fe98bc604945f30439706d350
Claudio Guevara <claudio.guevara@pharmkt.com>
2024-11-22 17:06:58 -0300

    /sw.js

--- src/swDev.js
+++ src/swDev.js
@@ -1,6 +1,6 @@
 export default function swDev() {
     // let swUrl = `${process.env.PUBLIC_URL}/sw.js`
-    const swUrl = "sw.js"
+    const swUrl = "/sw.js"
     
     navigator.serviceWorker.register(swUrl).then((response) => {
         console.warn("Response: ", response)
